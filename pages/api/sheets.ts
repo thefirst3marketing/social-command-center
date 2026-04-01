f54c349758ac679176802159a4299e88eccf899f
Tesia 💕 <tesia@Tesias-Air.lan>
2026-04-01 15:09:54 -0700

    fix google auth

--- pages/api/sheets.ts
+++ pages/api/sheets.ts
@@ -4,12 +4,11 @@ import path from 'path'
 
 const SHEET_ID = process.env.GOOGLE_SHEET_ID!
 
-const credentials = process.env.GOOGLE_SERVICE_ACCOUNT_JSON
-  ? JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON)
-  : require('../../google-credentials.json')
-
 const auth = new google.auth.GoogleAuth({
-  credentials,
+  credentials: process.env.GOOGLE_SERVICE_ACCOUNT_JSON
+    ? JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON)
+    : undefined,
+  keyFile: process.env.GOOGLE_SERVICE_ACCOUNT_JSON ? undefined : './google-credentials.json',
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 })
 
